8141bcce07a324ca3e521005a4056c0927bc9ccd
David Jangdal <djangdal@gmail.com>
2016-05-29 18:22:31 +0200

    Tracking time of each message

--- client.cpp
+++ client.cpp
@@ -4,8 +4,18 @@
 #include <arpa/inet.h>
 #include <thread>
 #include <vector>
+#include <sys/time.h>
 
 int socket_fd;
+
+struct Message {
+  std::string message;
+  struct timeval time_sent;
+  struct timeval time_received;
+  int time_taken;
+};
+
+std::vector<Message*> sent_messages;
 
 std::vector<std::string> parse_messages(char* buffer) {
   std::vector<std::string> messages;
@@ -20,6 +30,16 @@ std::vector<std::string> parse_messages(char* buffer) {
   }
   
   return messages;
+}
+
+Message* find_correct_message(std::string message_string) {
+  for (std::vector<Message*>::iterator i = sent_messages.begin(); i != sent_messages.end(); ++i) {
+    Message *message = *i;
+    if (message_string == message->message) {
+      return message;
+    }
+  }
+  return NULL;
 }
 
 void read_responses_thread() {
@@ -38,17 +58,20 @@ void read_responses_thread() {
 
     std::vector<std::string> messages = parse_messages(buffer);
     for (std::vector<std::string>::iterator i = messages.begin(); i != messages.end(); ++i) {
-      std::string message = *i;
-      std::cout << "Received: " << message << std::endl;
+
+      Message* message = find_correct_message(*i);
+      gettimeofday(&message->time_received, NULL);
+      message->time_taken = (message->time_sent.tv_sec - message->time_received.tv_sec) * 1000000 + (message->time_received.tv_usec - message->time_sent.tv_usec);
+      std::cout << "Received: " << message->message << " time taken " << message->time_taken << std::endl;
     }
   }
 }
 
-void send_message(std::string message) {
-  std::cout << "Sending: " << message << std::endl;
+void send_message(Message *message) {
+  std::cout << "Sending: " << message->message <<  std::endl;
 
-  message += "#"; //Adding delimiter
-  const char *m = message.c_str();
+  std::string message_delimiter = message->message + "#"; //Adding delimiter
+  const char *m = message_delimiter.c_str();
   if ((send(socket_fd, m, strlen(m), 0)) == -1) {
       fprintf(stderr, "Failure Sending Message");
       close(socket_fd);
@@ -87,8 +110,11 @@ int main(int argc, char const *argv[]) {
 
   // Sending 20 messages
   for (int i = 0; i < 20; ++i) {
-    std::string m = "message " + std::to_string(i);
-    send_message(m);
+    Message *message = new Message();
+    message->message = "message " + std::to_string(i);
+    gettimeofday(&message->time_sent, NULL);
+    sent_messages.push_back(message);
+    send_message(message);
   }
 
   // Waiting for all messages to get back
